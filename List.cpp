b14228cd33f84876bb205087ac9d6937ac48dbfa
IvanSemin33 <IvanSemin33@users.noreply.github.com>
2018-10-17 20:26:20 +0300

    3

--- List.cpp
+++ List.cpp
@@ -15,21 +15,15 @@ class List {
 public:
     List();
     List(const List&);
-    //~List();
+    ~List();
     int get_count();
     void insert(int pos = 0);
     void add_tail(Type n);
     void add_head(Type n);
-
-    // List& operator=(const List&);
-    // List& operator+(const List&);
-
+    void del_tail();
+    void del_head();
     void show();
-    // void show(int pos);
-
-
 };
-
 
 template <class Type>
 List<Type>::List() : Head(NULL),Tail(NULL), count(0) {}
@@ -40,6 +34,15 @@ List<Type>::List(const List &L) : Head(NULL),Tail(NULL), count(0){
     while (temp!=0){
         add_tail(temp->Val);
         temp=temp->Next;
+    }
+}
+
+template <class Type>
+List<Type>::~List() {
+    while (Head) {
+        Tail = Head->Next;
+        delete Head;
+        Head = Tail;
     }
 }
 
@@ -75,6 +78,24 @@ void List<Type>::add_tail(Type n) {
 }
 
 template <class Type>
+void List<Type>::del_head() {
+    Node<Type>* Del = Head;
+    Head = Head->Next;
+    Head->Prev = NULL;
+    delete Del;
+    count--;
+}
+
+template <class Type>
+void List<Type>::del_tail() {
+    Node<Type>* Del = Tail;
+    Tail = Tail->Prev;
+    Tail->Next = NULL;
+    delete Del;
+    count--;
+}
+
+template <class Type>
 void List<Type>::insert(int pos) {
     if(pos==0)
     {
@@ -96,7 +117,7 @@ void List<Type>::insert(int pos) {
         return;
     }
     int i=1;
-    Node<Type>* Ins=Head;
+    Node<Type>* Ins = Head;
     while (i<pos){
         Ins=Ins->Next;
         i++;
--- main.cpp
+++ main.cpp
@@ -5,21 +5,29 @@ using  namespace std;
 int main() {
 
     List<int> L;
+    cout<<"Addition:"<<endl;
+    L.add_tail(1);//push_back
+    L.show();
     L.add_tail(2);
     L.show();
-    L.add_tail(10);
+    L.add_head(3);//push_front
     L.show();
-    L.add_head(18);
+    L.add_head(4);
     L.show();
-
+    L.add_tail(5);
+    L.show();
+    cout<<"\nCount: "<<L.get_count()<<endl;
+    cout<<"\nInsert in position 3"<<endl;
     L.insert(3);//вставляем элемент на 3 позицию
     L.show();
-
-    List<int> L2(L);
+    cout<<"\nCopy:"<<endl;
+    List<int> L2(L);//создание списка с помощью копирование
     L2.show();
-
-    cout<<"Count: "<<L.get_count()<<endl;
-
-
+    cout<<"\nDeletion:"<<endl;
+    L2.del_head();//pop_front
+    L2.show();
+    L2.del_tail();//pop_back
+    L2.show();
+    cout<<"\nCount: "<<L2.get_count()<<endl;
     return 0;
 }
